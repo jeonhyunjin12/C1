9ab99fa027c76b1a4678b62295179837853b9022
jeonhyunjin12 <128660795+jeonhyunjin12@users.noreply.github.com>
2023-03-23 11:29:55 +0900

    Add files via upload

---
+++ C_4/2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+    int main()
+    {
+        char code1 = 'A';
+        char code2 = 65;
+    
+        printf("code1 = %c\n", code1);
+        printf("code2 = %c\n", code2+1);
+    
+      return 0;
+    }
